91a2c95aa1bdcb1e821f40094e09521e28934b58
Julio Martinez <julio.martinez@iudigital.edu.co>
2024-08-09 00:48:16 +0200

    plantilla inicial back

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('Hola LaSalle')
+const app = require('./app')
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+    console.log(`Servidor arrancó por puerto ${app.get('port')}`)
+})
