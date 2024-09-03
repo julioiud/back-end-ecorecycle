706515d4ce5c2e8347759a29569f2a1fca3d38b8
Sebastián Ramírez <sebaspagani@gmail.com>
2024-09-03 15:58:47 -0500

    Aumento base de datos canecas

--- create-collections/canecas.js
+++ create-collections/canecas.js
@@ -1,5 +1,4 @@
-db.canecas.insertMany([
-    {
+db.canecas.insertMany([{
         "ubicacion": "rectoria",
         "latitud": 6.2040175,
         "longitud": -75.5494086,
@@ -13,4 +12,278 @@ db.canecas.insertMany([
         "descripcion": "Al lado del baño de mujeres",
         "infoQR": "eco-recycle.bmj001"
     }
+]);
+db.canecas.insertMany([{
+        "ubicacion": "co-working",
+        "latitud": 6.202743,
+        "longitud": -75.546663,
+        "descripcion": "Afuera de Co-working",
+        "infoQR": "eco-recycle.cwg001"
+    },
+    {
+        "ubicacion": "corredor-robotica",
+        "latitud": 6.2040175,
+        "longitud": -75.5494086,
+        "descripcion": "Corredor Tercer piso afuera del salón Robótica",
+        "infoQR": "eco-recycle.cra001"
+    },
+    {
+        "ubicacion": "corredor-ascensor-oeste",
+        "latitud": 6.202807,
+        "longitud": -75.546860,
+        "descripcion": "Corredor Tercer piso al lado del ascensor-Este",
+        "infoQR": "eco-recycle.cao001"
+    },
+    {
+        "ubicacion": "corredor-ascensor-este",
+        "latitud": 6.202877,
+        "longitud": -75.546743,
+        "descripcion": "Corredor Tercer piso al lado del ascensor-Este",
+        "infoQR": "eco-recycle.cae001"
+    },
+    {
+        "ubicacion": "salon-1",
+        "latitud": 6.202441,
+        "longitud": -75.547118,
+        "descripcion": "Salón Oeste tercer piso bloque B bachillerato",
+        "infoQR": "eco-recycle.sca001"
+    },
+    {
+        "ubicacion": "salon-2",
+        "latitud": 6.202424,
+        "longitud": -75.546993,
+        "descripcion": "Salón 2 tercer piso bloque B bachillerato",
+        "infoQR": "eco-recycle.scb001"
+    },
+    {
+        "ubicacion": "salon-3",
+        "latitud": 6.202425,
+        "longitud": -75.546980,
+        "descripcion": "Salón 3 tercer piso bloque B bachillerato",
+        "infoQR": "eco-recycle.scc001"
+    },
+    {
+        "ubicacion": "salon-4",
+        "latitud": 6.202371,
+        "longitud": -75.546053,
+        "descripcion": "Salón 4 tercer piso bloque B bachillerato",
+        "infoQR": "eco-recycle.scd001"
+    },
+    {
+        "ubicacion": "salon-5",
+        "latitud": 6.202370,
+        "longitud": -75.546843,
+        "descripcion": "Salón Este tercer piso bloque B bachillerato",
+        "infoQR": "eco-recycle.sce001"
+    },
+    {
+        "ubicacion": "banos-bloque-b",
+        "latitud": 6.202440,
+        "longitud": -75.546683,
+        "descripcion": "Baños del Bloque B",
+        "infoQR": "eco-recycle.bbb001"
+    },
+    {
+        "ubicacion": "salones-columna-a",
+        "latitud": 6.202499,
+        "longitud": -75.547028,
+        "descripcion": "Salones Columna A Oeste Bloque B",
+        "infoQR": "eco-recycle.cas001"
+    },
+    {
+        "ubicacion": "salones-columna-b",
+        "latitud": 6.202473,
+        "longitud": -75.546960,
+        "descripcion": "Salones Columna B Bloque B",
+        "infoQR": "eco-recycle.cbs001"
+    },
+    {
+        "ubicacion": "salones-columna-c",
+        "latitud": 6.202458,
+        "longitud": -75.546899,
+        "descripcion": "Salones Columna C Bloque B",
+        "infoQR": "eco-recycle.ccs001"
+    },
+    {
+        "ubicacion": "salones-columna-d",
+        "latitud": 6.202444,
+        "longitud": -75.546838,
+        "descripcion": "Salones Columna D Bloque B",
+        "infoQR": "eco-recycle.cds001"
+    },
+    {
+        "ubicacion": "salones-columna-e",
+        "latitud": 6.202424,
+        "longitud": -75.546770,
+        "descripcion": "Salones Columna E Este Bloque B",
+        "infoQR": "eco-recycle.ces001"
+    },
+    {
+        "ubicacion": "trofeos",
+        "latitud": 6.202883,
+        "longitud": -75.546820,
+        "descripcion": "Caneca cerca de la vitrina de trofeos del colegio",
+        "infoQR": "eco-recycle.tfs001"
+    },
+    {
+        "ubicacion": "restaurante-afuera",
+        "latitud": 6.202883,
+        "longitud": -75.546820,
+        "descripcion": "Canecas afuera del restaruante",
+        "infoQR": "eco-recycle.rta001"
+    },
+    {
+        "ubicacion": "restaurante-adentro",
+        "latitud": 6.204010,
+        "longitud": -75.547261,
+        "descripcion": "Caneca dentro del restaurante",
+        "infoQR": "eco-recycle.rtd001"
+    },
+    {
+        "ubicacion": "bano-cancha-sintetica",
+        "latitud": 6.201734,
+        "longitud": -75.546454,
+        "descripcion": "Baños de la cancha sintética",
+        "infoQR": "eco-recycle.bcs001"
+    },
+    {
+        "ubicacion": "bano-hombres-coliseo",
+        "latitud": 6.202277,
+        "longitud": -75.546228,
+        "descripcion": "Baños del coliseo de hombres",
+        "infoQR": "eco-recycle.bhc001"
+    },
+    {
+        "ubicacion": "bano-mujeres-coliseo",
+        "latitud": 6.202173,
+        "longitud": -75.546263,
+        "descripcion": "Baños del coliseo de mujeres",
+        "infoQR": "eco-recycle.bmc001"
+    },
+    {
+        "ubicacion": "bano-mujeres-bachillerato-bloque-a",
+        "latitud": 6.202785,
+        "longitud": -75.546789,
+        "descripcion": "Baños de mujeres al lado de sala de profes",
+        "infoQR": "eco-recycle.bmp001"
+    },
+    {
+        "ubicacion": "bano-hombres-bachillerato-bloque-a",
+        "latitud": 6.202754,
+        "longitud": -75.546763,
+        "descripcion": "Baños de hombres al lado de sala de profes",
+        "infoQR": "eco-recycle.bhp001"
+    },
+    {
+        "ubicacion": "banos-puente-conexion-bloques",
+        "latitud": 6.202754,
+        "longitud": -75.546763,
+        "descripcion": "Baños de que quedan en la unión del bloque A con el Bloque B de bachillerato",
+        "infoQR": "eco-recycle.bpc001"
+    },
+    {
+        "ubicacion": "salones-columna-f",
+        "latitud": 6.202653,
+        "longitud": -75.546723,
+        "descripcion": "Salones Columna F Bloque A",
+        "infoQR": "eco-recycle.scf001"
+    },
+    {
+        "ubicacion": "salones-columna-g",
+        "latitud": 6.202706,
+        "longitud": -75.546750,
+        "descripcion": "Salones Columna G Bloque A",
+        "infoQR": "eco-recycle.scg001"
+    },
+    {
+        "ubicacion": "salones-columna-h",
+        "latitud": 6.202711,
+        "longitud": -75.546754,
+        "descripcion": "Salones Columna H Bloque A",
+        "infoQR": "eco-recycle.sch001"
+    },
+    {
+        "ubicacion": "salones-columna-i",
+        "latitud": 6.202780,
+        "longitud": -75.546810,
+        "descripcion": "Salones Columna I Bloque A",
+        "infoQR": "eco-recycle.sci001"
+    },
+    {
+        "ubicacion": "salones-columna-j",
+        "latitud": 6.202818,
+        "longitud": -75.546835,
+        "descripcion": "Salones Columna J Bloque A",
+        "infoQR": "eco-recycle.scj001"
+    },
+    {
+        "ubicacion": "salones-columna-k",
+        "latitud": 6.202881,
+        "longitud": -75.546716,
+        "descripcion": "Salones Columna K Bloque A",
+        "infoQR": "eco-recycle.sck001"
+    },
+    {
+        "ubicacion": "salones-columna-l",
+        "latitud": 6.202837,
+        "longitud": -75.54691,
+        "descripcion": "Salones Columna L Bloque A",
+        "infoQR": "eco-recycle.scl001"
+    },
+    {
+        "ubicacion": "salones-columna-m",
+        "latitud": 6.202797,
+        "longitud": -75.546656,
+        "descripcion": "Salones Columna M Bloque A",
+        "infoQR": "eco-recycle.scm001"
+    },
+    {
+        "ubicacion": "salones-columna-n",
+        "latitud": 6.202744,
+        "longitud": -75.546604,
+        "descripcion": "Salones Columna N Bloque A",
+        "infoQR": "eco-recycle.scn001"
+    },
+    {
+        "ubicacion": "salones-columna-o",
+        "latitud": 6.202711,
+        "longitud": -75.546577,
+        "descripcion": "Salones Columna O Bloque A",
+        "infoQR": "eco-recycle.sco001"
+    },
+    {
+        "ubicacion": "cafeteria-coliseo",
+        "latitud": 6.202450,
+        "longitud": -75.546469,
+        "descripcion": "Caneca al lado de la cafetería del coliseo",
+        "infoQR": "eco-recycle.cfc001"
+    },
+    {
+        "ubicacion": "cafeteria",
+        "latitud": 6.202910,
+        "longitud": -75.547067,
+        "descripcion": "Caneca de la cafetería principal del colegio",
+        "infoQR": "eco-recycle.cft001"
+    },
+    {
+        "ubicacion": "bano-mujeres-coliseo-afuera",
+        "latitud": 6.202167,
+        "longitud": -75.546375,
+        "descripcion": "Caneca afuera del coliseo y del baño de mujeres",
+        "infoQR": "eco-recycle.bma001"
+    },
+    {
+        "ubicacion": "poste-placas",
+        "latitud": 6.202208,
+        "longitud": -75.546471,
+        "descripcion": "Caneca en el poste de las placas de fútbol",
+        "infoQR": "eco-recycle.ppc001"
+    },
+    {
+        "ubicacion": "gradas-sintetica",
+        "latitud": 6.202208,
+        "longitud": -75.546471,
+        "descripcion": "Caneca de las gradas de la cancha sintética",
+        "infoQR": "eco-recycle.gds001"
+    },
 ]);
