d5b4875561dff30e0165b69b94e18951a60941d3
Julio Martinez <julio.martinez@iudigital.edu.co>
2024-09-15 22:57:45 +0200

    all

--- routes/caneca.js
+++ routes/caneca.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const { 
-    obtenerCanecas
+    obtenerCanecas,
+    validarQRCaneca
 } = require('../controllers/canecaController')
 const { validarToken } = require('../middlewares/validatoken')
 
 const router = Router()
 
 router.get('/', [validarToken], obtenerCanecas)
+router.get('/:info', [validarToken], validarQRCaneca)
 
 module.exports = router
